7fb4c3ba5f10469d538dac45538e6cd5b13b0eed
Administrator <gitlab@turrisystem.com>
2014-02-24 11:54:01 -0500

    merge linux and prompt repos

---
+++ view-colors.sh
@@ -0,0 +1,66 @@
+PS1="
+${RS} *******  RS  ${RS}
+ 
+${black} *******  black  ${RS}
+${red} *******  red  ${RS}
+${green} *******  green  ${RS}
+${yellow} *******  yellow  ${RS}
+${blue} *******  blue  ${RS}
+${purple} *******  purple  ${RS}
+${cyan} *******  cyan  ${RS}
+${white} *******  white  ${RS}
+ 
+${bblack} *******  bblack  ${RS}
+${bred} *******  bred  ${RS}
+${bgreen} *******  bgreen  ${RS}
+${byellow} *******  byellow  ${RS}
+${bblue} *******  bblue  ${RS}
+${bpurple} *******  bpurple  ${RS}
+${bcyan} *******  bcyan  ${RS}
+${bwhite} *******  bwhite  ${RS}
+ 
+${ublack} *******  ublack  ${RS}
+${ured} *******  ured  ${RS}
+${ugreen} *******  ugreen  ${RS}
+${uyellow} *******  uyellow  ${RS}
+${ublue} *******  ublue  ${RS}
+${upurple} *******  upurple  ${RS}
+${ucyan} *******  ucyan  ${RS}
+${uwhite} *******  uwhite  ${RS}
+ 
+${on_black} *******  on_black  ${RS}
+${on_red} *******  on_red  ${RS}
+${on_green} *******  on_green  ${RS}
+${on_yellow} *******  on_yellow  ${RS}
+${on_blue} *******  on_blue  ${RS}
+${on_purple} *******  on_purple  ${RS}
+${on_cyan} *******  on_cyan  ${RS}
+${on_white} *******  on_white  ${RS}
+ 
+${iblack} *******  iblack  ${RS}
+${ired} *******  ired  ${RS}
+${igreen} *******  igreen  ${RS}
+${iyellow} *******  iyellow  ${RS}
+${iblue} *******  iblue  ${RS}
+${ipurple} *******  ipurple  ${RS}
+${icyan} *******  icyan  ${RS}
+${iwhite} *******  iwhite  ${RS}
+ 
+${biblack} *******  biblack  ${RS}
+${bired} *******  bired  ${RS}
+${bigreen} *******  bigreen  ${RS}
+${biyellow} *******  biyellow  ${RS}
+${biblue} *******  biblue  ${RS}
+${bipurple} *******  bipurple  ${RS}
+${bicyan} *******  bicyan  ${RS}
+${biwhite} *******  biwhite  ${RS}
+ 
+${on_iblack} *******  on_iblack  ${RS}
+${on_ired} *******  on_ired  ${RS}
+${on_igreen} *******  on_igreen  ${RS}
+${on_iyellow} *******  on_iyellow  ${RS}
+${on_iblue} *******  on_iblue  ${RS}
+${on_ipurple} *******  on_ipurple  ${RS}
+${on_icyan} *******  on_icyan  ${RS}
+${on_iwhite} *******  on_iwhite  ${RS}
+"
